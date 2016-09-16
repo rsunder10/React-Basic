81fbac00df50f3187bf075ab6bf892f46ae4c94d
sunder <sunderis10@gmail.com>
2016-09-16 16:35:23 +0530

    React Using Youtube Api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+//Api for Youtube
+const API_KEY='AIzaSyCGJ1bRPnw1-uu2NpRuld7q_82nhbZUMiQ';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = { videos: [] };
+    //using npm for youtube search
+    YTSearch({key:API_KEY,term:'surfboards'},(videos) => {
+        this.setState({videos});
+        //ES6 will convert above statement to this.setState({videos:videos})
+    });
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  }
+  render(){
+    return(
+      <div>
+        <SearchBar />
+        <VideoDetail video = {this.state.videos[0]} />
+        <VideoList videos = {this.state.videos} />
+      </div>
+    )};
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
