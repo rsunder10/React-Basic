9597e109b40421b47b86585ea6323ba82cc15238
sunder <sunderis10@gmail.com>
2016-09-16 17:20:38 +0530

    State Change is Successfully established

--- src/index.js
+++ src/index.js
@@ -11,20 +11,30 @@ const API_KEY='AIzaSyCGJ1bRPnw1-uu2NpRuld7q_82nhbZUMiQ';
 class App extends Component{
   constructor(props){
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo:null
+     };
     //using npm for youtube search
     YTSearch({key:API_KEY,term:'surfboards'},(videos) => {
-        this.setState({videos});
-        //ES6 will convert above statement to this.setState({videos:videos})
+        this.setState({
+          videos:videos,
+          selectedVideo:videos[0]
+
+        });
+
     });
 
   }
+
   render(){
     return(
       <div>
         <SearchBar />
-        <VideoDetail video = {this.state.videos[0]} />
-        <VideoList videos = {this.state.videos} />
+        <VideoDetail video = {this.state.selectedVideo} />
+        <VideoList
+            onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
+            videos = {this.state.videos} />
       </div>
     )};
 }
